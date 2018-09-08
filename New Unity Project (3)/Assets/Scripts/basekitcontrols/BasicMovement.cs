197119fdf5cf26232412bbdfb0d4f1689d0f7672
Kent Reese <43098468+whenbellstoll@users.noreply.github.com>
2018-09-08 17:54:41 -0400

    Added a Physics Test Scene to Unity, just basic rigidbody stuff right now

---
+++ New Unity Project (3)/Assets/Scripts/basekitcontrols/BasicMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BasicMovement : MonoBehaviour
+{
+    
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float JumpForce;
+    [SerializeField]
+    private float WalkForce;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(  Input.GetKey(KeyCode.RightArrow) )
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            rb.AddForce(new Vector2(WalkForce, 0), ForceMode2D.Impulse);
+        }
+
+        if( Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            rb.AddForce(new Vector2(-WalkForce, 0), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+	}
+}
